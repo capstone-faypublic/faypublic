a820ccf342d304135e5ccfddb18479b8d037819b
jantimpe <timpe31@gmail.com>
2018-02-13 15:10:02 -0600

    Added equipment model

--- src/equipmentcheckout/models.py
+++ src/equipmentcheckout/models.py
@@ -3,6 +3,67 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 
+AUDIO = 'AUDIO'
+VIDEO = 'VIDEO'
+LIGHTING = 'LIGHTING'
+EQUIPMENT_CATEGORIES = [
+    (AUDIO, 'Audio'),
+    (VIDEO, 'Video'),
+    (LIGHTING, 'Lighting')
+]
+
+MICROPHONE = 'MICROPHONE'
+WIRELESS = 'WIRELESS'
+CASE = 'CASE'
+STAND = 'STAND'
+CABLE = 'CABLE'
+HEADPHONES = 'HEADPHONES'
+TELEPROMPTER = 'TELEPROMPTER'
+GREEN_SCREEN = 'GREEN_SCREEN'
+CAMERA = 'CAMERA'
+LIGHT_KIT = 'LIGHT_KIT'
+EQUIPMENT_SUBCATEGORIES = [
+    (MICROPHONE, 'Microphone'),
+    (WIRELESS, 'Wireless'),
+    (CASE, 'Case'),
+    (STAND, 'Stand'),
+    (CABLE, 'Cable'),
+    (HEADPHONES, 'Headphones'),
+    (TELEPROMPTER, 'Teleprompter'),
+    (GREEN_SCREEN, 'Green screen'),
+    (CAMERA, 'Camera'),
+    (LIGHT_KIT, 'Light kit')
+]
+
+CHECKOUT_24HR = 'CHECKOUT_24HR'
+CHECKOUT_WEEK = 'CHECKOUT_WEEK'
+CHECKOUT_TIMEFRAMES = [
+    (CHECKOUT_24HR, '24hr checkout'),
+    (CHECKOUT_WEEK, 'Thurs-Tues checkout')
+]
+
+class Equipment(models.Model):
+    quantity = models.IntegerField()
+    make = models.CharField(
+        max_length=255, 
+        null=True,
+        blank=False
+    )
+
+    model = models.CharField(
+        max_length=255, 
+        null=True,
+        blank=False
+    )
+
+    description = models.TextField(null=True, blank=True)
+
+    category = models.CharField(max_length=15, choices=EQUIPMENT_CATEGORIES, default=AUDIO)
+
+    sub_category = models.CharField(max_length=15, choices=EQUIPMENT_SUBCATEGORIES, default=MICROPHONE)
+
+    category = models.CharField(max_length=15, choices=CHECKOUT_TIMEFRAMES, default=CHECKOUT_WEEK)
+
 class EquipmentCheckout(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # equipmentID = models.ForeignKey(Equipment, on_delete=models.CASCADE)
