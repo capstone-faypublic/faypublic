461ea731b902aa237da32555cf26d7b01bb1d14a
jantimpe <timpe31@gmail.com>
2018-02-13 15:17:53 -0600

    Added equipment models to admin

--- src/equipmentcheckout/admin.py
+++ src/equipmentcheckout/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Equipment, EquipmentCheckout
 
 # Register your models here.
+class EquipmentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Equipment, EquipmentAdmin)
+
+class EquipmentCheckoutAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(EquipmentCheckout, EquipmentCheckoutAdmin)
