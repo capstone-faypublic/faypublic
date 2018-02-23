bbd34b7cd7ced14c1c2b273b5a314fcce2b32cd8
Garrett Graham <gwgraham@uark.edu>
2018-02-22 22:16:08 -0600

    added date picker for 'checkout_date' field for equipment checkouts

--- src/equipmentcheckout/forms.py
+++ src/equipmentcheckout/forms.py
@@ -1,7 +1,14 @@
 from django.forms import Form, ModelForm
 from .models import EquipmentCheckout
+from django import forms
+
+class DateInput(forms.DateInput):
+	input_type = 'date'
 
 class EquipmentCheckoutForm(ModelForm):
     class Meta:
         model = EquipmentCheckout
         exclude = ['user']
+        widgets = {
+        	'checkout_date' : DateInput(),
+        }
