9b7d18daa1b7395e1360fa5070e7460d72469b90
jantimpe <timpe31@gmail.com>
2018-03-10 13:30:24 -0600

    classes refactor

--- src/classes/models.py
+++ src/classes/models.py
@@ -6,22 +6,13 @@ from userprofile.models import Badge, User
 
 
 class Class(models.Model):
-    class_title = models.CharField(
-        max_length=255,
-        null=True,
-        blank=False,
-    )
+    class Meta:
+        verbose_name = "class"
+        verbose_name_plural = "classes"
 
-    class_description = models.TextField(
-        null=True,
-        blank=False
-    )
-
-    prereq_badges = models.ManyToManyField(Badge,
-        related_name="prerequisites",
-        blank=True
-    )
-
+    class_title = models.CharField(max_length=255, null=True, blank=False)
+    class_description = models.TextField(null=True, blank=False)
+    prerequisite_badges = models.ManyToManyField(Badge, related_name="prerequisites", null=True, blank=True)
     awarded_badges = models.ManyToManyField(Badge, related_name="awarded")
 
     def __str__(self):
@@ -30,22 +21,16 @@ class Class(models.Model):
     def __unicode__(self):
         return self.class_title
 
-    class Meta:
-        verbose_name_plural = "classes"
-
 
 class ClassSection(models.Model):
+    class Meta:
+        verbose_name = "section"
+        verbose_name_plural = "sections"
+
     class_key = models.ForeignKey(Class, on_delete=models.CASCADE)
-
     start_date = models.DateTimeField
-
     end_date = models.DateTimeField
-
-    seat_avaliable = models.IntegerField(
-        null=True,
-        blank=True,
-        default=6
-    )
+    seat_avaliable = models.IntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
         return self.class_key.class_title
@@ -55,18 +40,14 @@ class ClassSection(models.Model):
 
 
 class ClassRegistration(models.Model):
-    class_section = models.ForeignKey(ClassSection, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "registration"
+        verbose_name_plural = "registrations"
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    completed = models.BooleanField(
-        default=False
-    )
-
-    score = models.TextField(
-        default="0"
-    )
-
+    class_section = models.ForeignKey(ClassSection, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+    score_percentage = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=1)
 
     def __str__(self):
         return self.class_section.class_key.class_title
