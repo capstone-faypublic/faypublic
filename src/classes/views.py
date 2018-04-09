ed66f3a6f94a192ea2491610d3ce84581e196d9c
jantimpe <timpe31@gmail.com>
2018-04-09 10:04:16 -0500

    added check for class registration, error messages

--- src/classes/views.py
+++ src/classes/views.py
@@ -11,9 +11,18 @@ from userprofile.models import UserProfile
 # Create your views here.
 
 
+def user_registered_for_class_section(section, user):
+    for reg in section.classregistration_set.all():
+        if user == reg.user:
+            return True
+    return False
+
+
 @login_required
 def class_registration(request, slug):
     course = get_object_or_404(Class, slug=slug)
+
+    err_msg = ''
 
     if request.method == 'POST':
         section_id = request.POST.get('section_id')
@@ -25,21 +34,28 @@ def class_registration(request, slug):
             # check to make sure a user isn't already registered, it's really easy to reload the page and re-register for a course
 
             if userprofile.can_register_for_class(class_section.class_key):
-                reg = ClassRegistration(
-                    user=request.user,
-                    class_section=class_section,
-                )
-                reg.save()
+                if not user_registered_for_class_section(class_section, request.user):
+                    reg = ClassRegistration(
+                        user=request.user,
+                        class_section=class_section,
+                    )
+                    reg.save()
 
-                return redirect('user_classes')
-                # redirect to the profile and class registration list
-            # else: # use django flash messages api here for unsuccessful registration
+                    return redirect('user_classes')
+
+                else:
+                    err_msg = 'You are already registered for this section'
+            else:
+                err_msg = 'You have not met the prerequisites to take this class'
+        else:
+            err_msg = 'This class section is full'
 
     return render(
         request,
         'class_registration.html',
         context={
-            'class': course
+            'class': course,
+            'err_msg': err_msg
         }
     )
 
