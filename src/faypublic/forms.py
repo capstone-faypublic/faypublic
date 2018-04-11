b887e5d189c8a3d8a67345a5c6427c65729d3c1f
Garrett Graham <gwgraham@uark.edu>
2018-04-11 15:57:41 -0500

    Register form restyling

--- src/faypublic/forms.py
+++ src/faypublic/forms.py
@@ -12,3 +12,21 @@ class UserRegistrationForm(UserCreationForm):
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
         self.fields['email'].required = True
+
+        # Adding placeholder text
+        self.fields['username'].widget.attrs['placeholder'] = ('Username')
+        self.fields['first_name'].widget.attrs['placeholder'] = ('First Name')
+        self.fields['last_name'].widget.attrs['placeholder'] = ('Last Name')
+        self.fields['email'].widget.attrs['placeholder'] = ('Email')
+        self.fields['password1'].widget.attrs['placeholder'] = ('Password')
+        self.fields['password2'].widget.attrs['placeholder'] = ('Confirm Password')
+
+        # Removing help text
+        for fieldname in ['username', 'password2']:
+        	self.fields[fieldname].help_text = None
+        for fieldname in ['password1']:
+        	self.fields[fieldname].help_text = "Must contain 8 characters, with at least 1 letter"
+
+       	# Removing labels 
+        for fieldname in ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']:
+        	self.fields[fieldname].label = ""
