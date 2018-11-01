e935e69ba069221efeca41d8d255e5670a3d2b50
jantimpe <timpe31@gmail.com>
2018-11-01 18:28:22 -0500

    Update renewal script

--- renew-certs.sh
+++ renew-certs.sh
@@ -1,4 +1,4 @@
+docker-compose down
 letsencrypt renew
 cp -rL /etc/letsencrypt/live/login.faypublic.tv/* /etc/letsencrypt/copy
-docker-compose down
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
