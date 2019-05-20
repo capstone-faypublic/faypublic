8ff9d8048d5bdffb58f5e060df995340e4fd6c27
Jan Timpe <jantimpe@gmail.com>
2019-05-19 19:52:52 -0500

    updates for prod

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-domains=(jantimpe.dev)
+domains=(login.faypublic.tv)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="timpe31@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
