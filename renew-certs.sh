86f29929b6659dc162fd402c0ce98376a75a8192
Jan Timpe <jantimpe@gmail.com>
2019-12-07 13:57:28 -0600

    moved db into env

--- renew-certs.sh
+++
@@ -1,4 +0,0 @@
-docker-compose down
-letsencrypt renew
-cp -rL /etc/letsencrypt/live/login.faypublic.tv/* /etc/letsencrypt/copy
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
